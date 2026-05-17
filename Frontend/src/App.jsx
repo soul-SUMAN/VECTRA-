057d0c1765140317dc82b5f66a32286f1828a5a7
soul-SUMAN <sumanmondal1009@gmail.com>
2026-05-17 19:29:40 +0530

    refactor: restructure routing in App component and update main entry point

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,40 +1,16 @@
 
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-import Cars from "./pages/Cars.jsx";
-import Bookings from "./pages/MyBookings.jsx";
-import Wishlist from "./pages/Wishlist.jsx";
-import Admin from "./pages/Admin.jsx";
-import AdminCars from "./pages/AdminCars.jsx";
-import AdminBookings from "./pages/AdminBookings.jsx";
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './authentication/login.jsx'
-
-function App() {
+export default function App() {
   return (
-  <>
-   <Router>
-      <Navbar />
-     
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/cars" element={<AdminCars />} />
-        <Route path="/admin/bookings" element={<AdminBookings />} />
-
-      </Routes>
-      <Footer />
-    </Router>
-    
-  </>
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
   )
 }
 
-export default App
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -169,7 +169,7 @@ export default function Home() {
             </div>
             
             </div>
-
+            
         </div>
 
 
